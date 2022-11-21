b0564225a095948595abdf0d132d2f029e166405
hypernova7 <23711001+hypernova7@users.noreply.github.com>
2022-11-20 22:52:46 -0600

    Improvements and fixes
    Fix nginx config
    
    Fix scripts/check-for-updates.js
    `context.payload.head_commit` returns undefined in some cases

--- scripts/check-for-updates.js
+++ scripts/check-for-updates.js
@@ -1,19 +1,31 @@
-module.exports = async ({ context, core, exec }) => {
+module.exports = async ({ context, core, exec, github }) => {
   const limit = 1000 * 60 * 60 * 24; // 24hrs in milliseconds
-  const { timestamp, message } = context.payload.head_commit;
-  const forceDeploy = process.env.FORCE_DEPLOY || '';
+
+  // Get the date and message body of the last commit
+  const {
+    data: {
+      commit: {
+        message,
+        committer: { date: timestamp }
+      }
+    }
+  } = await github.rest.repos.getCommit({
+    ref: process.env.GITHUB_SHA,
+    owner: context.repo.owner,
+    repo: context.repo.repo
+  });
+  const forceDeploy =
+    (process.env.FORCE_DEPLOY && process.env.FORCE_DEPLOY === 'true') ||
+    message.startsWith('__deploy__');
   const date = new Date(timestamp).getTime();
 
   // Check if '__deploy__' prefix exists on commit message
   // or if process.env.FORCE_DEPLOY is set
   // And check if it hasn't been 24hrs since the last commit with '__deploy__' prefix,
   // to prevent multiple forced deployments, as this workflow runs every day at 12am UTC
-  if (
-    forceDeploy === 'true' || message.includes('__deploy__')
-      && Math.floor(Date.now() - date) < limit
-  ) {
+  if (forceDeploy && Math.floor(Date.now() - date) < limit) {
     core.setOutput('force_deploy', 'true');
-    core.warning('Forced deployment');
+    core.warning('A forced deployment will occur');
   }
 
   // Try to update the telegram-api-bot submodule,
@@ -25,4 +37,4 @@ module.exports = async ({ context, core, exec }) => {
       }
     }
   });
-}
+};
