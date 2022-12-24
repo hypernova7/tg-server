ad7cb538cc8ba2957773ab3ca3e19f2644d8a26e
hypernova7 <23711001+hypernova7@users.noreply.github.com>
2022-12-24 02:01:01 -0600

    Improve init-server.sh
    * Assign swap size using `SWAP_SIZE` environment variable

--- init-server.sh
+++ init-server.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env sh
 set -e
 
-# Increase swap size to %20 of available space (set the SWAP environment variable)
-# (this is only required on fly.io)
-if [[ ! -z "$SWAP" ]]; then
-  fallocate -l $(($(stat -f -c "(%a*%s/10)*2" .))) _swapfile
+# You can set the swap size if needed, just set the SWAP_SIZE environment variable, by default is 0
+# (You can even specify size in bytes)
+# Example: SWAP_SIZE=200M or SWAP_SIZE=4G or SWAP_SIZE=8589934592 (8589934592 = 8G)
+# You can use this prefixes:
+# - k or K = Kilobytes
+# - m o M = Megabytes
+# - g or G = Gigabytes
+SWAP_SIZE=${SWAP_SIZE:-0} # Set default value to 0 if SWAP_SIZE environment variable not exists
+
+# Convert human readable size to bytes
+# Source: https://stackoverflow.com/a/26621833
+size_to_bytes() {
+  echo $1 | awk \
+    '/[0-9]$/{print $1;next};
+    /[gG]$/{printf "%u", $1*(1024*1024*1024);next};
+    /[mM]$/{printf "%u", $1*(1024*1024);next};
+    /[kK]$/{printf "%u", $1*1024;next}'
+}
+
+SWAP_SIZE_IN_BYTES=`size_to_bytes $SWAP_SIZE`
+
+# Only setup swap if SWAP_SIZE_IN_BYTES IS greater than 0
+if [ $SWAP_SIZE_IN_BYTES -ne 0 ]; then
+  # Assign size to swap
+  fallocate -l $SWAP_SIZE _swapfile
+  # Setup swap
   mkswap _swapfile
+  # Enable swap
   swapon _swapfile
 fi
 
+# Replace environment variables
 envsub /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
+# Start services
 supervisord -c /etc/supervisor/supervisord.conf
