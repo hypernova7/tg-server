e9ac3d254936a0e75e5a01aa5670c176df698c53
Hussain <hussainabbas5416@outlook.com>
2022-12-29 11:53:05 +0530

    Comments improved

--- init-server.sh
+++ init-server.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 set -e
 
-# You can set the swap size if needed, just set the SWAP_SIZE environment variable, by default is 0
-# (You can even specify size in bytes)
+
+## ------------------------------------------------ SWAP FILE ------------------------------------------------ ##
+# You can set the swap size if needed, just set the SWAP_SIZE environment variable. (default 0)
 # Example: SWAP_SIZE=200M or SWAP_SIZE=4G or SWAP_SIZE=8589934592 (8589934592 = 8G)
-# You can use this prefixes:
+# Use these prefixes or in bytes:
 # - k or K = Kilobytes
 # - m o M = Megabytes
 # - g or G = Gigabytes
-SWAP_SIZE=${SWAP_SIZE:-0} # Set default value to 0 if SWAP_SIZE environment variable not exists
+SWAP_SIZE=${SWAP_SIZE:-0}                                                     # Default value is 0 if SWAP_SIZE ENV not set
 
-# Convert human readable size to bytes
-# Source: https://stackoverflow.com/a/26621833
+# Convert human readable size to bytes                                        (Source: https://stackoverflow.com/a/26621833)
 size_to_bytes() {
   echo $1 | awk \
     '/[0-9]$/{print $1;next};
@@ -19,20 +19,16 @@ size_to_bytes() {
     /[mM]$/{printf "%u", $1*(1024*1024);next};
     /[kK]$/{printf "%u", $1*1024;next}'
 }
-
 SWAP_SIZE_IN_BYTES=`size_to_bytes $SWAP_SIZE`
 
-# Only setup swap if SWAP_SIZE_IN_BYTES IS greater than 0
+# Setup swap file if SWAP_SIZE_IN_BYTES is greater than 0
 if [ $SWAP_SIZE_IN_BYTES -ne 0 ]; then
-  # Assign size to swap
-  fallocate -l $SWAP_SIZE _swapfile
-  # Setup swap
-  mkswap _swapfile
-  # Enable swap
-  swapon _swapfile
+  fallocate -l $SWAP_SIZE _swapfile                                             # Assign size to swap
+  mkswap _swapfile                                                              # Setup swap
+  swapon _swapfile                                                              # Enable swap
 fi
 
-# Replace environment variables
-envsub /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf
-# Start services
-supervisord -c /etc/supervisord.conf
+
+## ------------------------------------------------ PROGRAM ------------------------------------------------ ##
+envsub /etc/nginx/nginx.conf.tmpl > /etc/nginx/nginx.conf                       # Replace environment variables
+supervisord -c /etc/supervisord.conf                                            # Start services
